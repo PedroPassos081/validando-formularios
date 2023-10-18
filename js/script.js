b91f56ad0f0a1bd9ab96964e05531a468941da90
PedroPassos081 <pedro.passos081@gmail.com>
2023-10-18 09:09:25 -0300

    Adicionando o formulario para o localStorage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 import ehUmCPF from "./valida-cpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
 const camposDoFormulario = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario]")
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const listaRespostas = {
+    "nome": e.target.elements["nome"].value,
+    "email": e.target.elements["email"].value,
+    "rg": e.target.elements["rg"].value,
+    "cpf": e.target.elements["cpf"].value,
+    "aniversario": e.target.elements["aniversario"].value
+  }
+
+  localStorage.setItem("cadastro", JSON.stringify(listaRespostas))
+
+  window.location.href = './abrir-conta-form-2.html'
+})
+
 
 camposDoFormulario.forEach((campo) => {
   campo.addEventListener("blur", () => verificaCampo(campo));
@@ -58,7 +76,6 @@ function verificaCampo(campo) {
   tiposDeErro.forEach(erro => {
       if (campo.validity[erro]) {
           mensagem = mensagens[campo.name][erro];
-          console.log(mensagem);
       }
   }) 
   const mensagemErro = campo.parentNode.querySelector('.mensagem-erro')
